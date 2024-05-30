9bdcfc5c4ebe120de4e5065cb2566b35cb3f6c90
VanThen60hz <nguyenvthang2409@gmail.com>
2024-05-31 03:21:39 +0700

    feature: login & register ui

---
+++ src/pages/Authentication/Authentication.jsx
@@ -0,0 +1,38 @@
+import { Card, Grid } from "@mui/material";
+import Register from "./Register";
+
+const Authentication = () => {
+    return (
+        <div>
+            <Grid container>
+                <Grid
+                    className="h-screen flex justify-center items-center overflow-hidden"
+                    item
+                    xs={7}
+                >
+                    <img
+                        className="w-3/5"
+                        src="https://cdn.pixabay.com/photo/2019/09/14/09/44/cat-4475583_1280.png"
+                        alt=""
+                    />
+                </Grid>
+                <Grid item xs={5}>
+                    <div className="px-20 flex flex-col justify-center h-full">
+                        <Card className="card p-8">
+                            <div className="flex flex-col items-center mb-5 space-y-1">
+                                <h1 className="log">Meow Social</h1>
+                                <p className="text-center text-sm w-[70&]">
+                                    Ur Friendly Social Network
+                                </p>
+                            </div>
+                            {/* <Login /> */}
+                            <Register />
+                        </Card>
+                    </div>
+                </Grid>
+            </Grid>
+        </div>
+    );
+};
+
+export default Authentication;
---
+++ src/pages/Authentication/Register.jsx
@@ -0,0 +1,145 @@
+import {
+    Button,
+    FormControlLabel,
+    Radio,
+    RadioGroup,
+    TextField,
+} from "@mui/material";
+import { ErrorMessage, Field, Form, Formik } from "formik";
+import { useState } from "react";
+
+const initialValues = {
+    firstName: "",
+    lastName: "",
+    email: "",
+    password: "",
+    gender: "",
+};
+
+// const validationSchema = {
+//     email: Yup.string().email("Invalid email").required("Email is required"),
+//     password: Yup.string()
+//         .min(6, "Password must be at least 6 characters")
+//         .required("Password is required"),
+// };
+
+const Register = () => {
+    const [gender, setGender] = useState("");
+
+    const handleSubmit = (values) => {
+        values.gender = gender;
+        console.log("handle submit", values);
+    };
+
+    const handleChange = (event) => {
+        setGender(event.target.value);
+    };
+
+    return (
+        <>
+            <Formik
+                onSubmit={handleSubmit}
+                // validationSchema={validationSchema}
+                initialValues={initialValues}
+            >
+                <Form className="space-y-5">
+                    <div className="space-y-5">
+                        <div>
+                            <Field
+                                as={TextField}
+                                name="firstName"
+                                placeholder="First Name"
+                                type="text"
+                                variant="outlined"
+                                fullWidth
+                            />
+                            <ErrorMessage
+                                name="firstName"
+                                component="div"
+                                className="text-red-500"
+                            />
+                        </div>
+                        <div>
+                            <Field
+                                as={TextField}
+                                name="lastName"
+                                placeholder="Last Name"
+                                type="lastName"
+                                variant="outlined"
+                                fullWidth
+                            />
+                            <ErrorMessage
+                                name="lastName"
+                                component="div"
+                                className="text-red-500"
+                            />
+                        </div>
+                        <div>
+                            <Field
+                                as={TextField}
+                                name="email"
+                                placeholder="Email"
+                                type="email"
+                                variant="outlined"
+                                fullWidth
+                            />
+                            <ErrorMessage
+                                name="email"
+                                component="div"
+                                className="text-red-500"
+                            />
+                        </div>
+                        <div>
+                            <Field
+                                as={TextField}
+                                name="password"
+                                placeholder="Password"
+                                type="password"
+                                variant="outlined"
+                                fullWidth
+                            />
+                            <ErrorMessage
+                                name="password"
+                                component="div"
+                                className="text-red-500"
+                            />
+                        </div>
+
+                        <div className="flex justify-around">
+                            <RadioGroup
+                                row
+                                aria-label="gender"
+                                name="gender"
+                                onChange={handleChange}
+                            >
+                                <FormControlLabel
+                                    value="male"
+                                    name="gender"
+                                    control={<Radio />}
+                                    label="Male"
+                                />
+                                <FormControlLabel
+                                    value="female"
+                                    name="gender"
+                                    control={<Radio />}
+                                    label="Female"
+                                />
+                            </RadioGroup>
+                        </div>
+                    </div>
+                    <Button
+                        sx={{ padding: ".8rem 0rem" }}
+                        fullWidth
+                        type="submit"
+                        variant="contained"
+                        color="primary"
+                    >
+                        Register
+                    </Button>
+                </Form>
+            </Formik>
+        </>
+    );
+};
+
+export default Register;
